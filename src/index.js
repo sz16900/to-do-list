cad15a03d19e63803bb0aa5631b1149a1238b3a6
sz16900 <sz16900@my.bristol.ac.uk>
2020-06-25 17:38:42 -0500

    styles navbar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'bootstrap';
 import './scss/app.scss';
+import './css/main.css';
 
 // import Task from './task';
 // import Todo from './todos';
--- src/navbar.js
+++ src/navbar.js
@@ -2,12 +2,16 @@ const renderNavbar = () => {
   const container = document.getElementById('main-container');
   const nav = document.createElement('nav');
   nav.classList.add('navbar', 'navbar-dark', 'bg-dark');
+  const h1 = document.createElement('h1');
+  h1.classList.add('navbar-title');
+  h1.textContent = 'To-Do';
   const button = document.createElement('button');
   button.classList.add('navbar-toggler');
   const span = document.createElement('span');
   span.classList.add('navbar-toggler-icon');
   button.appendChild(span);
   nav.appendChild(button);
+  nav.appendChild(h1);
   container.appendChild(nav);
 
   return null;
