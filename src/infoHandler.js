425d9074eef549c5716c8b9e6010fb895d936897
sz16900 <sz16900@my.bristol.ac.uk>
2020-06-29 17:23:20 -0500

    :bug: adds content
    add to both all tasks and chosen project into local storage

--- src/infoHandler.js
+++ src/infoHandler.js
@@ -4,6 +4,9 @@ const setInfo = (task) => {
   const projects = JSON.parse(localStorage.getItem('projects'));
   //   add it to the all tasks project therefore [0]
   projects[0].tasks.push(task);
+  if (task.project !== 'All Tasks') {
+    projects.find((o) => o.name === task.project).tasks.push(task);
+  }
   localStorage.setItem('projects', JSON.stringify(projects));
   window.location.reload();
 };
@@ -13,7 +16,8 @@ const grabInfo = () => {
   const description = document.getElementById('description').value;
   const priority = document.getElementById('priority').value;
   const dueDate = document.getElementById('dueDate').value;
-  const task = Task(title, description, priority, dueDate);
+  const project = document.getElementById('project').value;
+  const task = Task(title, description, priority, dueDate, project);
   setInfo(task);
 };
 
